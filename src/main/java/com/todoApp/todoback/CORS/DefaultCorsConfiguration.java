c8c65623e7a880187a946b52b8bf919b77f4cf67
Mihkel Tiganik <mtiganik@gmail.com>
2023-09-10 17:39:50 +0300

    added CORS policy, ver 4

--- src/main/java/com/todoApp/todoback/CORS/DefaultCorsConfiguration.java
+++ src/main/java/com/todoApp/todoback/CORS/DefaultCorsConfiguration.java
@@ -3,15 +3,15 @@ package com.todoApp.todoback.CORS;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-
-@Configuration
-public class DefaultCorsConfiguration implements WebMvcConfigurer {
-
-    @Override
-    public void addCorsMappings(CorsRegistry registry){
-        registry.addMapping("/api/**")
-                .allowedMethods("GET","POST","PUT","DELETE")
-                .allowedHeaders("*")
-                .allowCredentials(false);
-    }
-}
+//
+//@Configuration
+//public class DefaultCorsConfiguration implements WebMvcConfigurer {
+//
+//    @Override
+//    public void addCorsMappings(CorsRegistry registry){
+//        registry.addMapping("/api/**")
+//                .allowedMethods("GET","POST","PUT","DELETE")
+//                .allowedHeaders("*")
+//                .allowCredentials(false);
+//    }
+//}
--- src/main/java/com/todoApp/todoback/CORS/ProdCorsConfiguration.java
+++ src/main/java/com/todoApp/todoback/CORS/ProdCorsConfiguration.java
@@ -5,13 +5,13 @@ import org.springframework.context.annotation.Profile;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-@Configuration
-@Profile("production")
-public class ProdCorsConfiguration implements WebMvcConfigurer {
-
-    @Override
-    public void addCorsMappings(CorsRegistry registry){
-        registry.addMapping("/api/**")
-                .allowedOrigins("https://mihkli-todo-app.netlify.app/");
-    }
-}
+//@Configuration
+//@Profile("production")
+//public class ProdCorsConfiguration implements WebMvcConfigurer {
+//
+//    @Override
+//    public void addCorsMappings(CorsRegistry registry){
+//        registry.addMapping("/api/**")
+//                .allowedOrigins("https://mihkli-todo-app.netlify.app/");
+//    }
+//}
